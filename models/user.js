b2afd22472ed3a1c2b43fc02f721c9024ad996a3
Ashley Smith <ash.mash.134@gmail.com>
2020-11-30 13:22:48 +1100

    Finalised fixing naming issues

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const User = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    uniqueCaseInsensitive: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    uniqueCaseInsensitive: true,
+    required: true,
+  },
+});
+
+User.plugin(passportLocalMongoose);
+// Apply the uniqueValidator plugin to userSchema.
+User.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", User);
